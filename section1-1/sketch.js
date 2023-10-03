b27c4f1d34107780ef268493adb7a9b548316230
skiii08 <146687825+skiii08@users.noreply.github.com>
2023-10-03 20:10:34 +0900

    Change section1-1

--- section1-1/sketch.js
+++ section1-1/sketch.js
@@ -8,10 +8,8 @@ function draw() {
   strokeWeight(4);     
   fill(127, 17, 131); 
   triangle(12,120,116,120,116,10); 
-  
   fill(240, 156, 180);    
   triangle(0,108,106,108,106,0);   
-  
   fill(255);    
   triangle(15,102,100,102,100,15);   
   fill(246,156,180);　         
