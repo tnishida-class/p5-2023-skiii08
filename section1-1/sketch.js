b81ecc94f9e64ed50a6d9488d6f6f40d42cfbab0
skiii08 <146687825+skiii08@users.noreply.github.com>
2023-10-03 20:08:27 +0900

    Change section1-1

--- section1-1/sketch.js
+++ section1-1/sketch.js
@@ -1,16 +1,22 @@
 function setup() {
-  createCanvas(128,128);
+  createCanvas(400, 400);
 }
 
 function draw() {
-  background(0);
-  strokeWeight(4);
-  fill(178, 100, 162);
-  triangle(12,120,116,120,116,10);
-  fill(94, 185, 84);
-  triangle(0,108,106,108,106,0);
-  fill(255);
-  textSize(32);
-  textFont("serif");
+  background(255);
+  stroke(255);
+  strokeWeight(4);     
+  fill(127, 17, 131); 
+  triangle(12,120,116,120,116,10); 
+  
+  fill(240, 156, 180);    
+  triangle(0,108,106,108,106,0);   
+  
+  fill(255);    
+  triangle(15,102,100,102,100,15);   
+  fill(246,156,180);　         
+  textSize(31);        
+  textFont("serif");   
   text("46", 68, 100);
+  text("櫻坂", 56,148)
 }
