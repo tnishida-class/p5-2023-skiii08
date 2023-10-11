1fb851865f8746c4298e34bafd10f2c6d04cafac
skiii08 <146687825+skiii08@users.noreply.github.com>
2023-10-11 11:18:17 +0900

    section2-q1change

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -1,11 +1,30 @@
 // チェッカー
 function setup() {
-  createCanvas(200, 200);
+  createCanvas(400, 400);
   let size = width / 8;
+  let d = size / 2;
   noStroke();
-  for(let i = 0; i < 8; i++){
-    for(let j = 0; j < 8; j++){
-      // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+  for(let s = 0; s < 8; s++){
+    for(let t = 0; t < 8; t++){
+     let x = s*size;
+     let y = t*size;
+     if ((s + t)%2 === 1){
+      fill(122);
+      rect(x,y,size,size);
+     if(((s+ t)%2 === 1) && (t<3)){
+      fill(255,0,0);
+      ellipse(x+d,y+d,size);}
+     if(((s+ t)%2 === 1) && (t>4)){
+        fill(0);
+        ellipse(x+d,y+d,size);
+      } 
+     }
     }
   }
-}
+}  
+      
+  
+
+
+     
+        
